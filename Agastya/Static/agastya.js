fc367e0fbc3199f68be41663c7c3922474054d2e
Biswajit <iambitmik@gmail.com>
2024-12-20 12:31:20 +0530

    Add welcome message and new styles; adjust color transitions

--- Agastya/Static/agastya.js
+++ Agastya/Static/agastya.js
@@ -4,12 +4,12 @@ const changeColor = () =>{
         setTimeout(()=>{
             const page1 = document.querySelector("#page1");
             page1.style.background="linear-gradient(to top,#f2f2f2,#555555)";            
-        },110)
+        },200)
         const headerElement = document.querySelector(".header");
-        headerElement.style.backgroundColor="black";
-
+        headerElement.style.backgroundColor="black"; 
         
-       
+        const mainBody = document.querySelector("#main");
+        mainBody.style.backgroundColor="black";
     }, 2000);
 }
 document.addEventListener("DOMContentLoaded", changeColor);
